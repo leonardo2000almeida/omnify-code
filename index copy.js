029545f66e2b7f2ab55c00171105d6c72f7ab3ad
Leonardo A R De Oliveira <leonardo.oliveira@negocioanimal.com.br>
2022-07-21 16:31:49 -0300

    feature: exclude index copy.js

--- index copy.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config();
-const { Server } = require("./src/interface/http/server.js");
-
-new Server(null, process.env.PORT).setupMiddlewares().setupRoutes().start();
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
 const { Server } = require("./src/interface/http/server.js");
 
-
-new Server(null, 3000).setupRoutes().setupMiddlewares().start();
+new Server(null, process.env.PORT).setupMiddlewares().setupRoutes().start();
